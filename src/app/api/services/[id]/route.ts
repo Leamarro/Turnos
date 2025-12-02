f371cf20145491112596b5e69e6b93710cbd1f20
Leandro Marrocchi <79809924+Leamarro@users.noreply.github.com>
2025-12-02 12:10:49 -0300

    Update route.ts

--- src/app/api/services/[id]/route.ts
+++ src/app/api/services/[id]/route.ts
@@ -1,6 +1,6 @@
 // /app/api/services/[id]/route.ts
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 
 export async function PUT(req: Request, { params }: { params: { id: string } }) {
   try {
