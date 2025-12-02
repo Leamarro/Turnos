8ed514ec97bdb4da3727539248b38a2d5faee05a
Leandro Marrocchi <79809924+Leamarro@users.noreply.github.com>
2025-12-02 13:40:39 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,10 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ReactNode } from "react";
 
-export default function RootLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="es">
-      <body>
+      <body className="min-h-screen">
         <Navbar />
         <div className="pt-16">{children}</div>
       </body>
