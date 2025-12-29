022487c8e98ac1614f4027e246e37378250c80ee
Leamarro <leaa.marrocchi@gmail.com>
2025-12-29 19:04:48 -0300

    fix ruta login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,20 @@
+"use client";
+
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ReactNode } from "react";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const pathname = usePathname(); // obtiene la ruta actual
+
+  const showNavbar = pathname !== "/login"; // ocultar navbar solo en login
+
   return (
     <html lang="es">
       <body className="min-h-screen">
-        <Navbar />
-        <div className="pt-16">{children}</div>
+        {showNavbar && <Navbar />}
+        <div className={showNavbar ? "pt-16" : ""}>{children}</div>
       </body>
     </html>
   );
